24c8c67866d3aad7bbdc5fe3e9b402263865a83f
Victor Rousseau <145038648+vicrou11@users.noreply.github.com>
2023-12-08 17:44:21 -0500

    Create SingletonPatternDemo.java

---
+++ Part1/SingletonPatternDemo.java
@@ -0,0 +1,13 @@
+package DesignFactors.src.part1;
+
+public class SingletonPatternDemo {
+
+	public static void main(String[] args) {
+		
+		SingleObject object = SingleObject.getInstance();
+		
+		object.showMessage();
+		
+	}
+	
+}
